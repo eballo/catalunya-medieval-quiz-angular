9c5127de1555298aa5d861e2ab4f56c0ea2ef7f0
eballo <enric.ballo@gmail.com>
2017-12-12 20:03:24 +0100

    rename app-root to catalunya-medieval-quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'catalunya-medieval-quiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
